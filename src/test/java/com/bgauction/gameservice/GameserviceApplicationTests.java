4614dac84c37197edd8c14a89903badc139e70c6
Lidija Silantjeva <lidija.silantjeva@accenture.com>
2024-10-21 16:59:53 +0300

    Game Service created

---
+++ src/test/java/com/bgauction/gameservice/GameserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgauction.gameservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
